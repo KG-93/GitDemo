e204391a0f5216c7b87e8365ca46e5a608a35d08
KG-93 <159827863+KG-93@users.noreply.github.com>
2024-03-10 22:15:22 +0530

    Delete invoking browser.py

--- invoking browser.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome import service
-
-service_obj = service()
-driver = webdriver.Chrome(service=service_obj)
-driver.get("https://google.co.im")
